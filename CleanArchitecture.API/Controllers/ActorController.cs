bb8e1ef64485c8a8f859a401946c47571f7c5a76
Lucas Ciminelli <lucasaciminelli@gmail.com>
2024-12-10 16:06:17 -0300

    Adding logic to ActorController controller

--- CleanArchitecture.API/Controllers/ActorController.cs
+++ CleanArchitecture.API/Controllers/ActorController.cs
@@ -1,4 +1,8 @@
 ﻿using CleanArchitecture.Application.Features.Actors.Commands.CreateActor;
+using CleanArchitecture.Application.Features.Actors.Commands.DeleteActor;
+using CleanArchitecture.Application.Features.Actors.Commands.UpdateActor;
+using CleanArchitecture.Application.Features.Actors.Queries.GetActorByNombre;
+using CleanArchitecture.Application.Features.Actors.Queries.GetActorsList;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -24,5 +28,57 @@ namespace CleanArchitecture.API.Controllers
         {
             return await _mediator.Send(command);
         }
+
+
+        [HttpPut(Name = "UpdateActor")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+
+        public async Task<ActionResult> UpdateActor([FromBody] UpdateActorCommand command)
+        {
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteActor")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+
+        public async Task<ActionResult> DeleteActor(int id)
+        {
+            var command = new DeleteActorCommand { Id = id };
+
+            await _mediator.Send(command);
+
+            return NoContent();
+
+        }
+
+
+        [HttpGet("username/{username}", Name = "GetActors")]
+        [ProducesResponseType(typeof(IEnumerable<ActorsVm>), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult<IEnumerable<ActorsVm>>> GetActors(string username)
+        {
+            var query = new GetActorListQuery(username);
+            var actors = await _mediator.Send(query);
+
+            return actors;
+        }
+
+        [HttpGet("nombre/{nombre}", Name = "GetActorByNombre")]
+        [ProducesResponseType(typeof(ActorsVm), (int)HttpStatusCode.OK)]
+        
+        public async Task<ActionResult<ActorsVm>> GetActorByNombre(string nombre)
+        {
+            var query = new GetActorByNombreQuery(nombre);
+            var actor = await _mediator.Send(query);
+
+            return actor;
+        }
+
     }
 }
